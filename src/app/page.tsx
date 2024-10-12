aba83f91e058ad254eb4a92da015e645e3b01d09
Takayuki Takahashi <ttkaws15@gmail.com>
2024-10-12 10:12:17 +0900

    css modules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,33 @@
 import Image from "next/image";
+import styles from './styles.module.css'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="w-full grid place-items-center">
-        <div className="grid w-40 h-40 place-items-center bg-gray-600 text-white"><span className="inline-block w-full text-center">this is</span> <br /><span className="inline-block w-full text-center">takayuki</span> <br /><span className="inline-block w-full text-center">takahashi</span></div>
-        <ul className="aa">
-          <li className="aaa">
-            <p className=""></p>
-            <p className=""></p>
-            <p className=""></p>
-            <p className=""></p>
-            <p className=""></p>
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <main className={styles.main}>
+        <div className={styles.titleContainer}>
+          <div className={styles.title}>
+            <p className={styles.titleText}>this is <br />takayuki takahashi</p>
+          </div>
+        </div>
+        <ul className={styles.blogList}>
+          <li className={styles.blogListItem}>
+            <p className={styles.blogListItemDate}>2024.10.12</p>
+            <p className={styles.blogListItemTitle}>ブログタイトルブログタイトルブログタイトルブログタイトル</p>
+            <p className={styles.blogListItemTag}>aaa</p>
+          </li>
+          <li className={styles.blogListItem}>
+            <p className={styles.blogListItemDate}>2024.10.12</p>
+            <p className={styles.blogListItemTitle}>ブログタイトルブログタイトルブログタイトル</p>
+            <p className={styles.blogListItemTag}>aaa</p>
+          </li>
+          <li className={styles.blogListItem}>
+            <p className={styles.blogListItemDate}>2024.10.12</p>
+            <p className={styles.blogListItemTitle}>ブログタイトルブログタイトル</p>
+            <p className={styles.blogListItemTag}>aaa</p>
           </li>
         </ul>
-       
-        {/* <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        /> */}
-        {/* <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol> */}
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          {/* <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a> */}
-        </div>
       </main>
-      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer> */}
     </div>
   );
 }
