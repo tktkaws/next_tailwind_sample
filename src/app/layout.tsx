bc9571043eb1d429ead5040a776a5c26deb90d69
Takayuki Takahashi <ttkaws15@gmail.com>
2024-10-18 07:12:49 +0900

    フォント変更 / リストのスタイリング調整 / framer-motionのテスト

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Header/>
         {children}
         <Footer/>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Image from "next/image";
 import styles from './styles.module.css'
+import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
@@ -10,46 +12,75 @@ export default function Home() {
             <p className={styles.titleText}>front <br />dispatch</p>
           </div>
         </div>
+        <div className='flex justify-center items-center'>
+        <motion.div
+   initial={{ x: -100 }}
+   animate={{ x: 200 }}
+   transition={{ duration: 2 }}
+   className='w-[170px] h-[170px] rounded-full bg-sky-500'>
+</motion.div>
+          <motion.div
+            initial={{ x: -100 }}
+            className='w-[170px] h-[170px] rounded-full bg-sky-500'>
+          </motion.div>
+          <motion.div
+   whileHover={{ opacity: 0 }}
+   className='w-[170px] h-[170px] rounded-full bg-sky-500'>
+</motion.div>
+<motion.div
+   whileTap={{ scale: 2 }}
+   className='w-[170px] h-[170px] rounded-full bg-sky-500'>
+</motion.div>
+        </div>
+        <p>
+          アクセシビリティ、モダンフロントエンド関係を中心とした技術ブログです。
+        </p>
+        <p>
+          自身の知識を整理することを第一の目的としています。
+        </p>
         <ul className={styles.blogList}>
           <li className={styles.blogListItem}>
             <p className={styles.blogListItemDate}>2024.10.12</p>
             <p className={styles.blogListItemTitle}>ブログタイトルブログタイトルブログタイトルブログタイトル</p>
-            <p className={styles.blogListItemTag}>aaa</p>
+            <p className={styles.blogListItemTag}>A11Y</p>
           </li>
           <li className={styles.blogListItem}>
             <p className={styles.blogListItemDate}>2024.10.12</p>
             <p className={styles.blogListItemTitle}>ブログタイトルブログタイトルブログタイトル</p>
-            <p className={styles.blogListItemTag}>aaa</p>
+            <p className={styles.blogListItemTag}>Java Script</p>
           </li>
           <li className={styles.blogListItem}>
             <p className={styles.blogListItemDate}>2024.10.12</p>
             <p className={styles.blogListItemTitle}>ブログタイトルブログタイトル</p>
-            <p className={styles.blogListItemTag}>aaa</p>
+            <p className={styles.blogListItemTag}>HTML</p>
           </li>
           <li className={styles.blogListItem}>
             <p className={styles.blogListItemDate}>2024.10.12</p>
             <p className={styles.blogListItemTitle}>ブログタイトルブログタイトル</p>
-            <p className={styles.blogListItemTag}>aaa</p>
+            <p className={styles.blogListItemTag}>CSS</p>
           </li>
           <li className={styles.blogListItem}>
             <p className={styles.blogListItemDate}>2024.10.12</p>
             <p className={styles.blogListItemTitle}>ブログタイトルブログタイトル</p>
-            <p className={styles.blogListItemTag}>aaa</p>
+            <div className={styles.blogListItemTagWrapper}>
+              <p className={styles.blogListItemTag}>A11Y</p>
+              <p className={styles.blogListItemTag}>Java Script</p>
+            </div>
           </li>
           <li className={styles.blogListItem}>
             <p className={styles.blogListItemDate}>2024.10.12</p>
             <p className={styles.blogListItemTitle}>ブログタイトルブログタイトル</p>
-            <p className={styles.blogListItemTag}>aaa</p>
+            <p className={styles.blogListItemTag}>A11Y</p>
           </li>
           <li className={styles.blogListItem}>
             <p className={styles.blogListItemDate}>2024.10.12</p>
             <p className={styles.blogListItemTitle}>ブログタイトルブログタイトル</p>
-            <p className={styles.blogListItemTag}>aaa</p>
+            <p className={styles.blogListItemTag}>A11Y</p>
           </li>
           <li className={styles.blogListItem}>
             <p className={styles.blogListItemDate}>2024.10.12</p>
             <p className={styles.blogListItemTitle}>ブログタイトルブログタイトル</p>
-            <p className={styles.blogListItemTag}>aaa</p>
+            <p className={styles.blogListItemTag}>A11Y</p>
           </li>
         </ul>
       </main>
