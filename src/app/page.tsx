89675bf148c32834131513b59ce9a664406c4fa0
tktkaws <ttkaws15@gmail.com>
2024-11-13 16:10:50 +0900

    実際の記事タイトルを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,120 +1,170 @@
-"use client";
-import Image from "next/image";
-import styles from './styles.module.css'
-import { motion } from 'framer-motion'
+import styles from "./styles.module.css";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
       <main className={styles.main}>
         <div className={styles.titleContainer}>
           <div className={styles.title}>
-            <p className={styles.titleText}>front <br />dispatch</p>
+            <p className={styles.titleText}>
+              front <br />
+              dispatch
+            </p>
           </div>
         </div>
-        {/* <div className='flex justify-center items-center'>
-                  <motion.div
-            initial={{ x: -100 }}
-            animate={{ x: 200 }}
-            transition={{ duration: 2 }}
-            className='w-[170px] h-[170px] rounded-full bg-sky-500'>
-          </motion.div>
-                    <motion.div
-                      initial={{ x: -100 }}
-                      className='w-[170px] h-[170px] rounded-full bg-sky-500'>
-                    </motion.div>
-                    <motion.div
-            whileHover={{ opacity: 0 }}
-            className='w-[170px] h-[170px] rounded-full bg-sky-500'>
-          </motion.div>
-          <motion.div
-            whileTap={{ scale: 2 }}
-            className='w-[170px] h-[170px] rounded-full bg-sky-500'>
-          </motion.div>
-        </div> */}
         <p>
           アクセシビリティ、モダンフロントエンド関係を中心とした技術ブログです。
         </p>
-        <p>
-          自身の知識を整理することを第一の目的としています。
-        </p>
+        <p>自身の知識を整理することを第一の目的としています。</p>
         <ul className={styles.tabList}>
-          <li className={styles.tabListItem}>
-            <a href="">ALL</a>
+          <li>
+            <a href="" className={styles.tabListItem}>ALL</a>
           </li>
-          <li className={styles.tabListItem}>
-            <a href="">A11Y</a>
+          <li>
+            <a href="" className={styles.tabListItem}>A11Y</a>
           </li>
-          <li className={styles.tabListItem}>
-            <a href="">HTML</a>
+          <li>
+            <a href="" className={styles.tabListItem}>HTML</a>
           </li>
-          <li className={styles.tabListItem}>
-            <a href="">CSS</a>
+          <li>
+            <a href="" className={styles.tabListItem}>CSS</a>
           </li>
-          <li className={styles.tabListItem}>
-            <a href="">Java Script</a>
+          <li>
+            <a href="" className={styles.tabListItem}>Java Script</a>
           </li>
         </ul>
         <ul className={styles.blogList}>
-          <li className={styles.blogListItem}>
+          <li>
+            <a href="" className={styles.blogListItem}>
+            <p className={styles.blogListItemTitle}>
+            【読了】Webアプリケーションアクセシビリティ
+            </p>
             <p className={styles.blogListItemDate}>2024.10.12</p>
-            <p className={styles.blogListItemTitle}>ブログタイトルブログタイトルブログタイトルブログタイトル</p>
             <div className={styles.blogListItemTagWrapper}>
               <p className={styles.blogListItemTag}>A11Y</p>
             </div>
+            <img src="/Arrow1.svg" alt="" className={styles.blogListItemArrow}/>
+            </a>
           </li>
-          <li className={styles.blogListItem}>
+          <li>
+            <a href="" className={styles.blogListItem}>
+            <p className={styles.blogListItemTitle}>
+              Webアクセシビリティに役立つアプリ
+            </p>
             <p className={styles.blogListItemDate}>2024.10.12</p>
-            <p className={styles.blogListItemTitle}>ブログタイトルブログタイトルブログタイトル</p>
-            <div className={styles.blogListItemTagWrapper}>
-              <p className={styles.blogListItemTag}>Java Script</p>
-            </div>
-          </li>
-          <li className={styles.blogListItem}>
-            <p className={styles.blogListItemDate}>2024.10.12</p>
-            <p className={styles.blogListItemTitle}>ブログタイトルブログタイトル</p>
-            <div className={styles.blogListItemTagWrapper}>
-            <p className={styles.blogListItemTag}>HTML</p>
-            </div>
-          </li>
-          <li className={styles.blogListItem}>
-            <p className={styles.blogListItemDate}>2024.10.12</p>
-            <p className={styles.blogListItemTitle}>ブログタイトルブログタイトル</p>
-            <div className={styles.blogListItemTagWrapper}>
-            <p className={styles.blogListItemTag}>CSS</p>
-            </div>
-          </li>
-          <li className={styles.blogListItem}>
-            <p className={styles.blogListItemDate}>2024.10.12</p>
-            <p className={styles.blogListItemTitle}>ブログタイトルブログタイトル</p>
             <div className={styles.blogListItemTagWrapper}>
               <p className={styles.blogListItemTag}>A11Y</p>
-              <p className={styles.blogListItemTag}>Java Script</p>
             </div>
+            <img src="/Arrow1.svg" alt="" className={styles.blogListItemArrow}/>
+            </a>
           </li>
-          <li className={styles.blogListItem}>
+          <li>
+            <a href="" className={styles.blogListItem}>
+            <p className={styles.blogListItemTitle}>
+            【備忘録】wai-ariaについて
+            </p>
             <p className={styles.blogListItemDate}>2024.10.12</p>
-            <p className={styles.blogListItemTitle}>ブログタイトルブログタイトル</p>
-             <div className={styles.blogListItemTagWrapper}>
+            <div className={styles.blogListItemTagWrapper}>
               <p className={styles.blogListItemTag}>A11Y</p>
-              <p className={styles.blogListItemTag}>Java Script</p>
             </div>
+            <img src="/Arrow1.svg" alt="" className={styles.blogListItemArrow}/>
+            </a>
           </li>
-          <li className={styles.blogListItem}>
+          <li>
+            <a href="" className={styles.blogListItem}>
+            <p className={styles.blogListItemTitle}>
+            Webアクセシビリティで参考になるサイト
+            </p>
             <p className={styles.blogListItemDate}>2024.10.12</p>
-            <p className={styles.blogListItemTitle}>ブログタイトルブログタイトル</p>
-             <div className={styles.blogListItemTagWrapper}>
+            <div className={styles.blogListItemTagWrapper}>
               <p className={styles.blogListItemTag}>A11Y</p>
-              <p className={styles.blogListItemTag}>Java Script</p>
             </div>
+            <img src="/Arrow1.svg" alt="" className={styles.blogListItemArrow}/>
+            </a>
           </li>
-          <li className={styles.blogListItem}>
+          <li>
+            <a href="" className={styles.blogListItem}>
+            <p className={styles.blogListItemTitle}>
+              Webアクセシビリティに力を入れている企業一覧
+            </p>
             <p className={styles.blogListItemDate}>2024.10.12</p>
-            <p className={styles.blogListItemTitle}>ブログタイトルブログタイトル</p>
-             <div className={styles.blogListItemTagWrapper}>
+            <div className={styles.blogListItemTagWrapper}>
               <p className={styles.blogListItemTag}>A11Y</p>
-              <p className={styles.blogListItemTag}>Java Script</p>
             </div>
+            <img src="/Arrow1.svg" alt="" className={styles.blogListItemArrow}/>
+            </a>
+          </li>
+          <li>
+            <a href="" className={styles.blogListItem}>
+            <p className={styles.blogListItemTitle}>
+            Webアクセシビリティの前提知識
+            </p>
+            <p className={styles.blogListItemDate}>2024.10.12</p>
+            <div className={styles.blogListItemTagWrapper}>
+              <p className={styles.blogListItemTag}>A11Y</p>
+            </div>
+            <img src="/Arrow1.svg" alt="" className={styles.blogListItemArrow}/>
+            </a>
+          </li>
+          <li>
+            <a href="" className={styles.blogListItem}>
+            <p className={styles.blogListItemTitle}>
+            業務歴1年目のコーダーがアクセシビリティ案件にアサインされた話
+            </p>
+            <p className={styles.blogListItemDate}>2024.10.12</p>
+            <div className={styles.blogListItemTagWrapper}>
+              <p className={styles.blogListItemTag}>A11Y</p>
+            </div>
+            <img src="/Arrow1.svg" alt="" className={styles.blogListItemArrow}/>
+            </a>
+          </li>
+          <li>
+            <a href="" className={styles.blogListItem}>
+            <p className={styles.blogListItemTitle}>
+            アクセシビリティチェックを実際にどう行う？
+            </p>
+            <p className={styles.blogListItemDate}>2024.10.12</p>
+            <div className={styles.blogListItemTagWrapper}>
+              <p className={styles.blogListItemTag}>A11Y</p>
+            </div>
+            <img src="/Arrow1.svg" alt="" className={styles.blogListItemArrow}/>
+            </a>
+          </li>
+          <li>
+            <a href="" className={styles.blogListItem}>
+            <p className={styles.blogListItemTitle}>
+            なぜアクセシビリティに関心があるのか
+            </p>
+            <p className={styles.blogListItemDate}>2024.10.12</p>
+            <div className={styles.blogListItemTagWrapper}>
+              <p className={styles.blogListItemTag}>A11Y</p>
+            </div>
+            <img src="/Arrow1.svg" alt="" className={styles.blogListItemArrow}/>
+            </a>
+          </li>
+          <li>
+            <a href="" className={styles.blogListItem}>
+            <p className={styles.blogListItemTitle}>
+            【読了】見えにくい、読みにくい「困った！」を解決するデザイン
+            </p>
+            <p className={styles.blogListItemDate}>2024.10.12</p>
+            <div className={styles.blogListItemTagWrapper}>
+              <p className={styles.blogListItemTag}>A11Y</p>
+            </div>
+            <img src="/Arrow1.svg" alt="" className={styles.blogListItemArrow}/>
+            </a>
+          </li>
+          <li>
+            <a href="" className={styles.blogListItem}>
+            <p className={styles.blogListItemTitle}>
+            【シリーズ】アクセシブルなUIを実装してみる【フォーム】
+            </p>
+            <p className={styles.blogListItemDate}>2024.10.12</p>
+            <div className={styles.blogListItemTagWrapper}>
+              <p className={styles.blogListItemTag}>A11Y</p>
+            </div>
+            <img src="/Arrow1.svg" alt="" className={styles.blogListItemArrow}/>
+            </a>
           </li>
         </ul>
       </main>
