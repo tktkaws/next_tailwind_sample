dcaae3f35736ba9135427362d14ede1b67dee1a7
tktkaws <ttkaws15@gmail.com>
2024-11-15 12:19:37 +0900

    microcmsから取得した記事を表示

--- src/app/component/Article/index.tsx
+++ src/app/component/Article/index.tsx
@@ -1,7 +1,7 @@
 // import Image from 'next/image';
-import type { Blog } from '@/app/_libs/microcms';
+import type { Blog } from "@/app/_libs/microcms";
 // import Category from '../Category';
-import styles from './index.module.css';
+import index from "./index.module.css";
 
 type Props = {
   data: Blog;
@@ -9,15 +9,58 @@ type Props = {
 
 export default function Article({ data }: Props) {
   return (
-    <main>
-      <h1 className={styles.title}>{data.title}</h1>
-      <p>{data.publishedAt ?? data.createdAt}</p>
-      <div
-        className={styles.content}
-        dangerouslySetInnerHTML={{
-          __html: data.body,
-        }}
-      />
-    </main>
+    <div className="font-[family-name:var(--font-geist-sans)]">
+      <main className={index.main}>
+        <div className={index.blogTitleContainer}>
+                <div className={index.blogTitleContainerInner}>
+                    <div className={index.blogTitle}>
+                        <p className={index.blogTitleText}>{data.title}</p>
+                    </div>
+                    <p className={index.blogDate}>{data.publishedAt ?? data.createdAt}</p>
+                    <p className={index.blogTag}>A11Y</p>
+                </div>
+        </div>
+        <article className={index.article}>
+                <div className={index.articleMainContainer}>
+                <div
+          className={index.content}
+          dangerouslySetInnerHTML={{
+            __html: data.body,
+          }}
+        />
+                   
+                    <h2 className={index.heading}>関連記事</h2>
+                    <ul className={index.blogList}>
+                        <li className={index.blogListItem}>
+                            <p className={index.blogListItemDate}>2024.10.12</p>
+                            <p className={index.blogListItemTitle}>ブログタイトルブログタイトルブログタイトルブログタイトル</p>
+                            <p className={index.blogListItemTag}>aaa</p>
+                        </li>
+                        <li className={index.blogListItem}>
+                            <p className={index.blogListItemDate}>2024.10.12</p>
+                            <p className={index.blogListItemTitle}>ブログタイトルブログタイトルブログタイトル</p>
+                            <p className={index.blogListItemTag}>aaa</p>
+                        </li>
+                        <li className={index.blogListItem}>
+                            <p className={index.blogListItemDate}>2024.10.12</p>
+                            <p className={index.blogListItemTitle}>ブログタイトルブログタイトル</p>
+                            <p className={index.blogListItemTag}>aaa</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className={index.articleListContainer}>
+                    <ul className={index.articleList}>
+                        <li><a href=""></a>目次1</li>
+                        <li><a href=""></a>目次2</li>
+                        <li><a href=""></a>目次3</li>
+                        <li><a href=""></a>目次4</li>
+                        <li><a href=""></a>目次5</li>
+                        <li><a href=""></a>目次6</li>
+                    </ul>
+                </div>
+            </article>
+        
+      </main>
+    </div>
   );
 }
