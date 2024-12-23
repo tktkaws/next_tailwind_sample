f53a946c01dd8aa73730368bd26d6cb443039ab2
tktkaws <ttkaws15@gmail.com>
2024-12-23 13:28:30 +0900

    Articleコンポーネント

--- src/app/component/Article/index.tsx
+++ src/app/component/Article/index.tsx
@@ -1,78 +1,29 @@
-// import Image from 'next/image';
-import type { Blog } from "@/app/_libs/microcms";
-// import Category from '../Category';
+import type { Blog, getBlogsList } from "@/app/_libs/microcms";
 import index from "./index.module.css";
 import "github-markdown-css";
+import { renderToc } from "@/app/_libs/render-toc";
+import TableOfContents from "@/app/component/TableOfContents";
 
 type Props = {
   data: Blog;
 };
 
-export default function Article({ data }: Props) {
+export default async function Article({ data }: Props) {
+  const toc = renderToc(data.body);
+
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      <main className={index.main}>
-        <div className={index.blogTitleContainer}>
-          <div className={index.blogTitleContainerInner}>
-            <div className={index.blogTitle}>
-              <p className={index.blogTitleText}>{data.title}</p>
-            </div>
-            <p className={index.blogDate}>
-              {data.publishedAt ?? data.createdAt}
-            </p>
-            <p className={index.blogTag}>A11Y</p>
-          </div>
-        </div>
-        <article className={index.article}>
-          <div className={index.articleMainContainer}>
-            <div
-              className="markdown-body"
-              dangerouslySetInnerHTML={{
-                __html: data.body,
-              }}
-            />
-            <h2 className={index.heading}>関連記事</h2>
-            <ul className={index.blogList}>
-              <li className={index.blogListItem}>
-                <p className={index.blogListItemDate}>2024.10.12</p>
-                <p className={index.blogListItemTitle}>
-                  ブログタイトルブログタイトルブログタイトルブログタイトル
-                </p>
-                <p className={index.blogListItemTag}>aaa</p>
-              </li>
-              <li className={index.blogListItem}>
-                <p className={index.blogListItemDate}>2024.10.12</p>
-                <p className={index.blogListItemTitle}>
-                  ブログタイトルブログタイトルブログタイトル
-                </p>
-                <p className={index.blogListItemTag}>aaa</p>
-              </li>
-            </ul>
-          </div>
-          <div className={index.articleListContainer}>
-            <ul className={index.articleList}>
-              <li>
-                <a href=""></a>目次1
-              </li>
-              <li>
-                <a href=""></a>目次2
-              </li>
-              <li>
-                <a href=""></a>目次3
-              </li>
-              <li>
-                <a href=""></a>目次4
-              </li>
-              <li>
-                <a href=""></a>目次5
-              </li>
-              <li>
-                <a href=""></a>目次6
-              </li>
-            </ul>
-          </div>
-        </article>
-      </main>
-    </div>
+    <article className={index.article}>
+      <div className={index.articleMainContainer}>
+        <div
+          className="markdown-body"
+          dangerouslySetInnerHTML={{
+            __html: data.body,
+          }}
+        />
+      </div>
+      <div className={index.articleListContainer}>
+        <TableOfContents toc={toc} />
+      </div>
+    </article>
   );
 }
