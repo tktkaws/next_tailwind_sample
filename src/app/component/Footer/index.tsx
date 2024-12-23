41e305d6b08935a0b61965bc1b0b3a804bb23e97
tktkaws <ttkaws15@gmail.com>
2024-12-23 13:27:27 +0900

    Footerコンポーネント

---
+++ src/app/component/Footer/index.tsx
@@ -0,0 +1,12 @@
+import styles from './index.module.css'
+
+export default function Footer() {
+    return (
+      <>
+          <footer className={styles.footer}>
+              <div className={styles.copyLight}>© Takayuki Takahashi</div>
+              <div></div>
+          </footer>
+      </>
+    );
+  }
