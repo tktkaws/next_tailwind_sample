0392b6ec019b1dfb134e0cec709e83736cbeaf29
tktkaws <ttkaws15@gmail.com>
2024-12-26 16:10:36 +0900

    デジタル庁オマージュ

--- src/app/component/Header/index.tsx
+++ src/app/component/Header/index.tsx
@@ -1,18 +1,26 @@
-import styles from './index.module.css'
+import styles from "./index.module.css";
+import { useCallback } from "react";
 
 export default function Header() {
-    return (
-      <>
-         <header className={styles.header}>
-            <div className={styles.logo}><a href="/">Front Dispatch</a></div>
-            {/* <div className={styles.headerTitle}>WAI-ARIAについての備忘録</div> */}
-            <nav className={styles.nav}>
-                <ul className={styles.navList}>
-                    <li><a href="/">home</a></li>
-                    <li><a href="/about/">about</a></li>
-                </ul>
-            </nav>
-        </header>
-      </>
-    );
-  }
+ 
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <a href="/">Front Dispatch</a>
+        </div>
+        <nav className={styles.nav}>
+          <ul className={styles.navList}>
+            <li>
+              <a href="/">blog</a>
+            </li>
+            <li>
+              <a href="/about/">about</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      
+    </>
+  );
+}
