3773c3ff4cdf00280f87024bcade6a0101d4f9b3
Takayuki Takahashi <ttkaws15@gmail.com>
2025-01-07 07:16:38 +0900

    レイアウトのリファクタリング

--- src/app/component/TitleContainer/index.tsx
+++ src/app/component/TitleContainer/index.tsx
@@ -2,21 +2,19 @@ import styles from "./index.module.css";
 
 export default function TitleContainer() {
   return (
-    <>
       <div className={styles.titleContainer}>
         <div className={styles.title}>
           <p className={styles.titleText}>
-          front <br />
-          dispatch
-            </p>
-          </div>
+            front <br />
+            dispatch
+          </p>
         </div>
         <div className={styles.lead}>
-          <p>
-            アクセシビリティ、モダンフロントエンド関係を中心とした技術ブログです。
-          </p>
+        <p>
+          アクセシビリティ、モダンフロントエンド関係を中心とした技術ブログです。
+        </p>
         <p>自身の知識を整理することを第一の目的としています。</p>
       </div>
-    </>
+      </div>
   );
 }
